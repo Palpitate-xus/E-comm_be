54b713e96000d8405d056ed9aa68063ad62edc79
Palpitate <xus1758011080@outlook.com>
2023-06-26 16:24:04 +0800

    更新生成token生成机制

--- utils/auth.py
+++ utils/auth.py
@@ -1,11 +1,12 @@
 import jwt
 from datetime import datetime, timedelta
 
-#生成token
+# 生成token
 def generate_token(username):
+    secret_key = "your_secret_key"  # 密钥
     payload = {
         "username": username,
         "exp": datetime.utcnow() + timedelta(hours=24)  # 令牌过期时间
     }
-    token = jwt.encode(payload, "your_secret_key", algorithm="HS256")
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
     return token

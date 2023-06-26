a9c4fca691625856464ce3d6f6c9fb58c05a14e7
zyf <3043863274@qq.com>
2023-06-26 16:17:09 +0800

    登陆并返回token 分模块

--- main.py
+++ main.py
@@ -3,48 +3,6 @@ from routers.login import router as login_router
 
 app = FastAPI()
 app.include_router(login_router)
-#
-# # 数据库连接信息
-# DB_CONFIG = {
-#     'host': 'localhost',
-#     'port': 3306,
-#     'user': 'root',
-#     'password': 'Zyf1231234321',
-#     'database': 'E-commerce_platform',
-# }
-
-# # 创建数据库连接
-# connection = pymysql.connect(**DB_CONFIG)
-
-#
-# @app.on_event("shutdown")
-# def shutdown_event():
-#     # 关闭数据库连接
-#     connection.close()
-#
-#
-# @app.post("/login")
-# async def login(username: str, password: str):
-#     # 创建游标对象
-#     with connection.cursor() as cursor:
-#         # 查询数据库中是否存在匹配的用户名和密码
-#         query = "SELECT * FROM users WHERE username=%s AND password=%s"
-#         cursor.execute(query, (username, password))
-#         result = cursor.fetchone()
-#
-#         if result is None:
-#             # 用户名或密码不匹配，返回错误响应
-#             raise HTTPException(status_code=401, detail="Invalid username or password")
-#         else:
-#             # 生成JWT令牌
-#             payload = {
-#                 "username": username,
-#                 "exp": datetime.utcnow() + timedelta(hours=1)  # 令牌过期时间
-#             }
-#             token = jwt.encode(payload, "your_secret_key", algorithm="HS256")
-#
-#             # 返回包含令牌的成功响应
-#             return {"token": token}
 
 
 if __name__ == "__main__":

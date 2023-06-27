6a9ac63f37f5cde54e796fb99683e9d35d6c5aa4
Palpitate <xus1758011080@outlook.com>
2023-06-27 15:06:53 +0800

    抽离出数据库config

---
+++ db/config.py
@@ -0,0 +1,8 @@
+# 数据库连接信息
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'password': '123456789',
+    'database': 'E-commerce_platform',
+}
--- db/database.py
+++ db/database.py
@@ -1,13 +1,6 @@
 import pymysql
 from models.user import User
-# 数据库连接信息
-DB_CONFIG = {
-    'host': 'localhost',
-    'port': 3306,
-    'user': 'root',
-    'password': '123456789',
-    'database': 'E-commerce_platform',
-}
+from .config import DB_CONFIG
 #建立连接
 def get_connection():
     return pymysql.connect(**DB_CONFIG)

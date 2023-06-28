8c033ccd6dcf3445448c6507c1b0c3f6b025ecee
董旭 <11904383+DX2001@user.noreply.gitee.com>
2023-06-28 09:53:26 +0800

    剥离database中用户表的操作，按照接口规范实现了用户接口文档中第一个表的所有功能

--- db/db_supplier.py
+++ db/db_supplier.py
@@ -1,6 +1,10 @@
 from models.supplier import Supplier
+from models.product import Product
 from .database import execute_query,get_connection,close_connection
+from datetime import datetime
 
 def accept_order(supplier:Supplier):
     #将订单数据加入至数据库中
-    sql="INSERT INTO "
+    sql="INSERT INTO Supply_order(supplier_id,product_id,quantity,storage_time) VALUES (%s %s %s %s)"
+    storage_time=datetime.now()
+    result=execute_query(sql,(Supplier.supplier_id, Product.product_id, Product.stock_quantity, storage_time))

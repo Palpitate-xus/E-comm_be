f8c96da2f4ece5751ba32765c23dc9304390d3cc
zyf <3043863274@qq.com>
2023-06-30 17:17:22 +0800

    rsa

--- routers/userrouter/rsa.py
+++ routers/userrouter/rsa.py
@@ -5,7 +5,7 @@ from fastapi import Request
 router = APIRouter()
 
 
-@router.post("/publicKey")
+@router.post("/api/publicKey")
 def rsa():
     return {"code": 200, "msg": 'success', "data": {"mockServer": "true", "publicKey":'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBT2vr+dhZElF73FJ6xiP181txKWUSNLPQQlid6DUJhGAOZblluafIdLmnUyKE8mMHhT3R+Ib3ssZcJku6Hn72yHYj/qPkCGFv0eFo7G+GJfDIUeDyalBN0QsuiE/XzPHJBuJDfRArOiWvH0BXOv5kpeXSXM8yTt5Na1jAYSiQ/wIDAQAB'}
 }

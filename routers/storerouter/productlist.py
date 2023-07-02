3f6caaaf0e7e7bf646932c3dd4983de0430f1977
zyf <3043863274@qq.com>
2023-07-02 10:52:44 +0800

    修复了productlist的bug

--- routers/storerouter/productlist.py
+++ routers/storerouter/productlist.py
@@ -11,13 +11,16 @@ router = APIRouter()
 @router.post("/api/store/productlist/")
 def productlist(request: Request):
     token = request.headers.get("Authorization")
-    result1 = db_user.user_info(token)
-    for item in result1:
-        if item[6] == 'admin':
-            result = db_store.productlist1()
-        if item[6] == 'customer':
-            result = db_store.productlist()
-            print(result)
+    try:
+        result1 = db_user.user_info(token)
+        for item in result1:
+            if item[6] == 'admin':
+                result = db_store.productlist1()
+            if item[6] == 'customer':
+                result = db_store.productlist()
+                print(result)
+    except:
+        result = db_store.productlist()
     # 获取商品列表
     data = []
 

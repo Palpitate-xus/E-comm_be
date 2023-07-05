8dbde9598ff58d2372dce01c102e65809a749f41
zyf <3043863274@qq.com>
2023-07-05 20:58:49 +0800

    修复了一些bug

--- routers/userrouter/userorder/create_order.py
+++ routers/userrouter/userorder/create_order.py
@@ -15,5 +15,5 @@ def create_order(createorder: CreateOrder, request: Request):
     if result == 0:
         return {"code": 403, "message": "Order create failed", "data": {}}
     else:
-        qr_coderesult = qr_code.generate_qr_code("http://mall.iyigui.cn:8000/api/payments/qrcode_pay/"  + result[0][0])
+        qr_coderesult = qr_code.generate_qr_code("http://mall.iyigui.cn:8000/api/payments/qrcode_pay/"  + result)
         return {"code": 200, "message": "Order create successfully", "data": {"qr_code": qr_coderesult}}

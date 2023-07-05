e4a9eb3e0e4c4dec875fd5681d593d6d8eebf3cc
zyf <3043863274@qq.com>
2023-07-05 14:37:20 +0800

    修复了一些bug

--- db/db_product.py
+++ db/db_product.py
@@ -1,7 +1,8 @@
 #对商品表插入数据、删除数据和修改数据
 from models.storemodel.product import Product
-from .database import execute_query,get_connection,close_connection
-
+from db.database import get_connection, close_connection, execute_query
+from utils.auth import decode_token
+from datetime import datetime
 def delete_product(product:Product):
     connection = get_connection()
     try:
@@ -25,13 +26,20 @@ def edit_product(product:Product):
                            product.product_price,product.category,
                            product.product_image,product.product_status,product.product_id))
 
-def insert_product(product:Product):
+def insert_product(product:Product, token):
+    sql = "SELECT supplier_id FROM Supplier WHERE user_id = %s"
+    result = execute_query(sql, (decode_token(token, "your_secret_key")['userid']))
+    connection = get_connection()
+    cur = connection.cursor()
     sql = """
             INSERT INTO product
             (product_id, product_name, product_description, product_price, category, product_image, product_status)
             VALUES
             (%s, %s, %s, %s, %s, %s, %s)
         """
-    execute_query(sql,(product.product_id,product.product_name,product.product_description,
+    cur.execute(sql,(product.product_id,product.product_name,product.product_description,
                            product.product_price,product.category,product.product_image,
                            'active'))
+    lid = cur.lastrowid
+    sql_insert = "INSERT INTO inventory (supplier_id, product_id, inventory_time, stock_quantity) VALUES (%s, %s, %s, %s)"
+    execute_query(sql_insert,(result[0][0], lid, datetime.now(), product.stock_quantity))
--- routers/storerouter/insert_product.py
+++ routers/storerouter/insert_product.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter
 from db import db_product
 from models.storemodel.product import Product
 router=APIRouter()
-
+from fastapi import Request
 @router.post("/api/store/insert_product")
-def insert_product(product:Product):
-    db_product.insert_product(product)
+def insert_product(request: Request, product:Product):
+    token = request.headers.get("Authorization")
+    db_product.insert_product(product, token)
     return {"code": 200, "message": "Product insert successful", "data": {}}
 

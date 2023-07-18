7df981483e5dffc108f03d7dc5d157e420780ca4
xus <xus1758011080@outlook.com>
2023-07-18 09:05:54 +0800

    Update config.py

--- db/config.py
+++ db/config.py
@@ -3,7 +3,7 @@ DB_CONFIG = {
     'host': 'localhost',
     'port': 3306,
     'user': 'root',
-    'password': 'Zyf1231234321',
+    'password': 'password',
     'database': 'E-commerce_platform',
     'autocommit': True
 }
